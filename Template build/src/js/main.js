a203204ca0d9c3f8e7d3d49e7dfda06490dc4bbf
Tyron Wytrykowski <tyronwyt@gmail.com>
2017-10-01 22:58:51 +0100

    Update file structure

---
+++ Template build/src/js/main.js
@@ -0,0 +1,5 @@
+var mobileBtn = document.getElementById('mobile-nav');
+mobileBtn.addEventListener("click", function(){
+  var nav = document.getElementById('nav');
+  nav.setAttribute("style", "display: block");
+})
